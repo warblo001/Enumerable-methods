984c390aef22c1c310def5069c945fc77a1601f2
warblo001 <67009988+warblo001@users.noreply.github.com>
2020-11-24 00:02:58 +0100

    add more methods
    add my_select and my_all?

--- main.rb
+++ main.rb
@@ -11,5 +11,24 @@ module Enumerable
     end
   end
 
+  def my_select
+    n_arr = []
+    self.my_each do |y|
+      n_arr.push(y) if yield(y)
+    end
+    n_arr
+  end
+
+  def my_all?
+    x = 0
+    while x < self.size
+      unless yield(self[x])
+        return false
+      end
+    x += 1
+    end
+    true
+  end
+
   
 end
